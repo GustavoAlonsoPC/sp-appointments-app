f6413b334426dac7ea05fc401317904aba62886b
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-05 15:16:51 -0500

    creating and routing screens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './screens/home/home.component';
+import { AppointmentsComponent } from './screens/appointments/appointments.component';
+import { TestsComponent } from './screens/tests/tests.component';
+import { AffiliatesComponent } from './screens/affiliates/affiliates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'appointments',
+    component: AppointmentsComponent
+  },
+  {
+    path: 'tests',
+    component: TestsComponent
+  },
+  {
+    path: 'affiliates',
+    component: AffiliatesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HomeComponent } from './screens/home/home.component';
+import { TestsComponent } from './screens/tests/tests.component';
+import { AffiliatesComponent } from './screens/affiliates/affiliates.component';
+import { AppointmentsComponent } from './screens/appointments/appointments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    TestsComponent,
+    AffiliatesComponent,
+    AppointmentsComponent
   ],
   imports: [
     BrowserModule,

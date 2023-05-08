0c031a1b2719da13344737f79d710deff767e670
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-08 17:36:23 -0500

    create data models

---
+++ src/app/shared/models/affiliate.model.ts
@@ -0,0 +1,6 @@
+export interface Affiliate {
+    id: number;
+    name: string;
+    mail: string;
+    age: number;
+}
---
+++ src/app/shared/models/appointment.model.ts
@@ -0,0 +1,7 @@
+export interface Appointment {
+    id: number;
+    dateAppointment: Date;
+    hourAppointment: string;
+    idTest: number;
+    idAffiliate: number;
+}

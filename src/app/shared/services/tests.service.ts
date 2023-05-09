cf4fbd54b14cb0bdbcda1a0f508efa7a616d8951
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-09 09:59:40 -0500

    Test service return tests collection

--- src/app/shared/services/tests.service.ts
+++ src/app/shared/services/tests.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Test } from '../models/test.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestsService {
 
-  constructor() { }
+  private testUrl = "/api/controller/tests";
+  constructor(private http: HttpClient) { }
+
+  getAllTests() {
+    return this.http.get<Test[]>(this.testUrl);
+  }
 }

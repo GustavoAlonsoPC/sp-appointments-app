6626e2b1bc746889a71e1f1d7ddd68ba00acbd9c
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-09 16:46:58 -0500

    show all affiliates

--- src/app/screens/affiliates/affiliates.component.ts
+++ src/app/screens/affiliates/affiliates.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Affiliate } from 'src/app/shared/models/affiliate.model';
+import { Item } from 'src/app/shared/models/item.model';
+import { AffiliatesService } from 'src/app/shared/services/affiliates.service';
 
 @Component({
   selector: 'app-affiliates',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AffiliatesComponent {
 
+  allAffiliates: Affiliate[] = [];
+  attrs = ['Id', 'Nombre', 'Edad', 'Correo'];
+
+  constructor(private service: AffiliatesService) {}
+
+  ngOnInit(): void {
+    this.service.getAllAffiliates().subscribe(data => {
+      this.allAffiliates = data;
+    });
+  }
+
+  itemType(): Item<Affiliate> {
+    return {
+      listingItems: this.allAffiliates
+    };
+  }
 }

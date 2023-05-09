8891ec6dfd86f66854c63a97f4f2501548ae9728
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-09 10:05:20 -0500

    using listing template

--- src/app/screens/tests/tests.component.ts
+++ src/app/screens/tests/tests.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/shared/models/item.model';
+import { Test } from 'src/app/shared/models/test.model';
+import { TestsService } from 'src/app/shared/services/tests.service';
 
 @Component({
   selector: 'app-tests',
   templateUrl: './tests.component.html',
   styleUrls: ['./tests.component.css']
 })
-export class TestsComponent {
+export class TestsComponent implements OnInit {
+  allTests: Test[] = [];
+  attrs = ['Id', 'Nombre', 'Descripción'];
 
+  constructor(private service: TestsService) {}
+
+  ngOnInit(): void {
+    this.service.getAllTests().subscribe(data => {
+      this.allTests = data;
+    });
+  }
+
+  itemType(): Item<Test> {
+    return {
+      listingItems: this.allTests
+    };
+  }
 }

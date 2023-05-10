b259afe434b300322a9420a69c3a346c45bbbb5c
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-10 15:06:30 -0500

    home view completed

--- src/app/screens/home/main-list/main-list.component.ts
+++ src/app/screens/home/main-list/main-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import { Affiliate } from 'src/app/shared/models/affiliate.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-main-list',
@@ -14,12 +17,92 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   ]
 })
 export class MainListComponent {
-  dataSource = ELEMENT_DATA;
+  /* dataSource = ELEMENT_DATA;
   columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
-  expandedElement!: PeriodicElement | null;
+  expandedElement!: PeriodicElement | null; */
+ /*  dataSource = new MatTableDataSource<MyData>(MY_DATA);
+  displayedColumns = ['name', 'age'];
+  expandedElement!: MyData | null;
+
+  toggleRow(element: MyData) {
+    this.expandedElement = this.expandedElement === element ? null : element;
+  } */
+  dataSource = ELEMENT_DATA;
+  displayedColumns = ['id', 'name', 'age', 'mail'];
+  columnsToDisplayWithExpand = ['expand', ...this.displayedColumns];
+  expandedElement!: DataSource | null;
 }
 
+export interface DataSource extends Affiliate {
+  nestedData: CustomAppointmentsDetails[];
+}
+
+export interface CustomAppointmentsDetails {
+  id: number;
+  dateAppointment: Date;
+  hourAppointment: string;
+  testName: string;
+}
+
+const ELEMENT_DATA: DataSource[] = [
+  {
+    id: 1,
+    name: 'Aff1',
+    mail: 'aff1@mail.com',
+    age: 24,
+    nestedData: [
+      {
+        id: 1,
+        dateAppointment: new Date(),
+        hourAppointment: '12:00',
+        testName: 'test1'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Aff2',
+    mail: 'aff2@mail.com',
+    age: 45,
+    nestedData: [
+      {
+        id: 2,
+        dateAppointment: new Date(),
+        hourAppointment: '12:00',
+        testName: 'test2'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Aff3',
+    mail: 'aff3@mail.com',
+    age: 98,
+    nestedData: [
+      {
+        id: 3,
+        dateAppointment: new Date(),
+        hourAppointment: '12:00',
+        testName: 'test3'
+      }
+    ]
+  }
+];
+
+/* 
+export interface MyData {
+  name: string;
+  age: number;
+}
+
+const MY_DATA: MyData[] = [
+  {name: 'Alice', age: 25},
+  {name: 'Bob', age: 35},
+  {name: 'Charlie', age: 58},
+]
+ */
+/* 
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -55,66 +138,55 @@ const ELEMENT_DATA: PeriodicElement[] = [
         silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
         lightest solid element.`,
   },
-  {
-    position: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    symbol: 'Be',
-    description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`,
-  },
-  {
-    position: 5,
-    name: 'Boron',
-    weight: 10.811,
-    symbol: 'B',
-    description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`,
-  },
-  {
-    position: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    symbol: 'C',
-    description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`,
-  },
-  {
-    position: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    symbol: 'N',
-    description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`,
-  },
-  {
-    position: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    symbol: 'O',
-    description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`,
-  },
-  {
-    position: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    symbol: 'F',
-    description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`,
-  },
-  {
-    position: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    symbol: 'Ne',
-    description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`,
-  },
 ];
+
+export interface CustomAppointmentDetails {
+  id: number;
+  dateAppointment: Date;
+  hourAppointment: string;
+  testName: string;
+}
+
+const ELEMENT_DATA_AFF: Affiliate[] = [
+  {
+    id: 1,
+    name: 'aff1',
+    mail: 'aff1@mail.com',
+    age: 50
+  },
+  {
+    id: 2,
+    name: 'aff2',
+    mail: 'aff2@mail.com',
+    age: 51
+  },
+  {
+    id: 3,
+    name: 'aff3',
+    mail: 'aff3@mail.com',
+    age: 52
+  }
+]
+
+const ELEMENT_DATA_APP: CustomAppointmentDetails[] = [
+  {
+    
+      id: 1,
+      dateAppointment: new Date(),
+      hourAppointment: '12:00',
+      testName: 'test1'
+  },
+  {
+      id: 2,
+      dateAppointment: new Date(),
+      hourAppointment: '12:00',
+      testName: 'test2'
+  },
+  {
+      id: 3,
+      dateAppointment: new Date(),
+      hourAppointment: '12:00',
+      testName: 'test3'
+  },
+]
+ */

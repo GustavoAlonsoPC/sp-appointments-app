ebe3216270d2af648f2f1c195dd70e07b26eca80
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-10 18:21:25 -0500

    create a common view header

--- src/app/shared/components/view-header/view-header.component.ts
+++ src/app/shared/components/view-header/view-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-view-header',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ViewHeaderComponent {
 
+  @Input() viewTitle: string = '';
 }

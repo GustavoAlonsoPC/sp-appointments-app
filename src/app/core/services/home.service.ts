8c6d87c88fdb08a007b0f71e759bf1563f3bf746
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-15 11:46:16 -0500

    home service first attempt

--- src/app/core/services/home.service.ts
+++ src/app/core/services/home.service.ts
@@ -1,9 +1,71 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AffiliatesService } from './affiliates.service';
+import { AppointmentsService } from './appointments.service';
+import { TestsService } from './tests.service';
+import { Affiliate } from 'src/app/shared/models/affiliate.model';
+import { Observable } from 'rxjs';
+import { Appointment } from 'src/app/shared/models/appointment.model';
+import { CustomAppointmentsDetails } from 'src/app/shared/models/custom-appointments-detals.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  affiliatesList: Affiliate[] = [];
+  affiliateAppointmentsList: Appointment[] = [];
+  appointmentResponse: CustomAppointmentsDetails[] = [];
+  testsNames: string[] = [];
+
+  constructor(
+    private http: HttpClient, 
+    private affiliates: AffiliatesService, 
+    private appointments: AppointmentsService,
+    private tests: TestsService) {
+      
+      this.getAllAffiliates().subscribe(data => {
+        this.affiliatesList = data;
+      });
+     }
+
+    getAllAffiliates(): Observable<Affiliate[]> {
+      return this.affiliates.getAll();
+    }
+
+    getAllAffiliateAppointments(idAffiliate: number) {
+      return this.appointments.getByAffiliateId(idAffiliate).subscribe(data => {
+        this.affiliateAppointmentsList = data;
+      });
+    }
+  
+    getTestById(idTest: number) {
+      return this.tests.getById(idTest);
+    }
+
+    extractTestsNames() {
+      this.affiliateAppointmentsList.forEach(app => {
+        this.getTestById(app.idTest).subscribe(data => {
+          this.testsNames.push(data.name);
+        });
+      })
+    }
+
+    mapAppointmentsResponse() {
+      this.extractTestsNames();
+      this.affiliateAppointmentsList.forEach((app, idx) => {
+        this.appointmentResponse.push({
+          id: app.id,
+          dateAppointment: app.dateAppointment,
+          hourAppointment: app.hourAppointment,
+          testName: this.testsNames[idx]
+        })
+      })
+    }
+
+    clickAff(idAffiliate: number) {
+      this.getAllAffiliateAppointments(idAffiliate);
+      this.mapAppointmentsResponse();
+      return this.appointmentResponse;
+    }
 }

fda7ae570bbbb1f1d9d69462379d294fc09c5387
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-16 09:35:10 -0500

    extract all id available

--- src/app/shared/layouts/crud/appointments-form/appointments-form.component.ts
+++ src/app/shared/layouts/crud/appointments-form/appointments-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AffiliatesService } from 'src/app/core/services/affiliates.service';
+import { TestsService } from 'src/app/core/services/tests.service';
 
 @Component({
   selector: 'app-appointments-form',
@@ -8,4 +10,23 @@ import { Component } from '@angular/core';
 export class AppointmentsFormComponent {
   name = '';
   description = '';
+
+  affiliatesIdList: number[] = [];
+  testIdList: number[] = [];
+
+  constructor(
+    private affiliates: AffiliatesService, 
+    private tests: TestsService
+    ) {
+      this.affiliates.getAll().subscribe(affs => {
+        affs.forEach(a => this.affiliatesIdList.push(a.id))
+      })
+
+      this.tests.getAll().subscribe(ts => {
+        ts.forEach(t => this.testIdList.push(t.id))
+      })
+
+      console.log('aff Ids', this.affiliatesIdList)
+      console.log('tests Ids', this.testIdList)
+    }
 }

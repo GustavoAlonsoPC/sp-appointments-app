dbf6d77f487e7d4ed92f9dfbfd718c8165dd6808
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-19 12:50:13 -0500

    return a success dialog

--- src/app/core/services/dialog.service.ts
+++ src/app/core/services/dialog.service.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationComponent } from 'src/app/shared/components/dialogs/confirmation/confirmation.component';
 import { ConfirmDialogData } from '../models/confirm-dialog-data.model';
 import { Observable } from 'rxjs';
+import { SuccessDialogData } from '../models/success-dialog-data.model';
+import { SuccessComponent } from 'src/app/shared/components/dialogs/success/success.component';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,14 @@ export class DialogService {
 
   confirmDialog(data: ConfirmDialogData): Observable<boolean> {
     return this.dialog.open(ConfirmationComponent,{
+      data,
+      width: '400px',
+      disableClose: true
+    }).afterClosed()
+  }
+
+  successDialog(data: SuccessDialogData): Observable<boolean> {
+    return this.dialog.open(SuccessComponent, {
       data,
       width: '400px',
       disableClose: true

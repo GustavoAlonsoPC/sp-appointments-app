78b5dbc12f8bb7e32d63f1ee0235a0b233fee6a6
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-22 12:04:55 -0500

    success: test form component extends from abstract crud form

--- src/app/shared/layouts/crud/tests-form/tests-form.component.ts
+++ src/app/shared/layouts/crud/tests-form/tests-form.component.ts
@@ -1,44 +1,47 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TestsService } from 'src/app/core/services/tests/tests.service';
 import { Test } from 'src/app/core/models/test.model';
 import { DialogService } from 'src/app/core/services/dialog.service';
 import { Router } from '@angular/router';
 import { DATA_SAVE_CONFIRMATION, DATA_SAVE_ERROR, DATA_SAVE_SUCCESS, DATA_UPDATE_CONFIRMATION, DATA_UPDATE_ERROR, DATA_UPDATE_SUCCESS } from 'src/app/core/utils/dialogs-data';
+import { AbstractCrudForm } from 'src/app/core/base/abstract-crud-form';
+import { ManagerService } from 'src/app/core/services/manager.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-tests-form',
   templateUrl: './tests-form.component.html',
   styleUrls: ['./tests-form.component.css']
 })
-export class TestsFormComponent implements OnInit {
+export class TestsFormComponent extends AbstractCrudForm implements OnInit {
 
-  @Input() register: Test = {
+  @Input() data: Test = {
     id: 0,
     name: '',
     description: ''
   }
 
-  form!: FormGroup;
-
   constructor(
     private fb: FormBuilder,
-    private service: TestsService,
-    private dialog: DialogService,
-    private router: Router
-    ) {}
+    private s: ManagerService,
+    d: DialogService
+    ) {
+    super(s, d, new Router());
+  }
   ngOnInit(): void {
     this.form = this.initForm();
+    this.register = <Test> this.data;
   }
 
   initForm(): FormGroup {
     return this.fb.group({
-      name: new FormControl(this.register.name, [Validators.required]),
-      description: new FormControl(this.register.description, [Validators.required, Validators.maxLength(100)])
+      name: new FormControl(this.data.name, [Validators.required]),
+      description: new FormControl(this.data.description, [Validators.required, Validators.maxLength(100)])
     })
   }
 
-  onSubmit() {
+  /* onSubmit() {
     if (this.register.id === 0) {
       this.dialog.confirmDialog(DATA_SAVE_CONFIRMATION).subscribe(r => {
         if (!r) return;
@@ -65,5 +68,5 @@ export class TestsFormComponent implements OnInit {
       })
     }
   }
-  redirect = () => this.router.navigate(['/tests']);
+  redirect = () => this.router.navigate(['/tests']); */
 }

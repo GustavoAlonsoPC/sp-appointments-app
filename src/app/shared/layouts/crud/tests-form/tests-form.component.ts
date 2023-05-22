ed4d23dae058a70b6fda303b7130ad446e793fe4
Gustavo Penaranda <gustavo.penarada@sophossolutions.com>
2023-05-22 09:37:43 -0500

    imp save and update dialogs in tests

--- src/app/shared/layouts/crud/tests-form/tests-form.component.ts
+++ src/app/shared/layouts/crud/tests-form/tests-form.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TestsService } from 'src/app/core/services/tests/tests.service';
 import { Test } from 'src/app/core/models/test.model';
+import { DialogService } from 'src/app/core/services/dialog.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tests-form',
@@ -20,7 +22,9 @@ export class TestsFormComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private service: TestsService
+    private service: TestsService,
+    private dialog: DialogService,
+    private router: Router
     ) {}
   ngOnInit(): void {
     this.testForm = this.initForm();
@@ -34,7 +38,58 @@ export class TestsFormComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.register.id === 0) this.service.save(this.testForm.value).subscribe(r => console.log(r))
-    else this.service.update(this.testForm.value, this.register.id).subscribe(r => console.log(r))
+    if (this.register.id === 0) {
+      this.dialog.confirmDialog({
+        title: 'Guardar registro',
+        message: 'Estás a punto de guardar un nuevo registro, ¿seguro(a) de continuar?',
+        confirmText: 'Continuar',
+        cancelText: 'Cancelar'
+      }).subscribe(r => {
+        if (!r) return;
+
+        this.service.save(this.testForm.value).subscribe({next: (r) => {
+            if(r) this.dialog.successDialog({
+              title: 'Guardado exitoso',
+              message: 'Se ha guardado un nuevo registro',
+              acceptText: 'Aceptar'
+            }).subscribe(s => {if(s) this.redirect()})
+          }, error: (e) => {
+            console.log(e)
+            this.dialog.errorDialog({
+              title: 'Ha ocurrido un error',
+              message: 'Tu registro no se ha podido guardar',
+              acceptText: 'Aceptar'
+            }).subscribe(s => {if(s) this.redirect()})
+          }
+        })
+      })
+    } else {
+      //this.service.update(this.testForm.value, this.register.id).subscribe()
+      this.dialog.confirmDialog({
+        title: 'Actualizar registro',
+        message: 'Estás a punto de actualizar un registro, ¿seguro(a) de continuar?',
+        confirmText: 'Continuar',
+        cancelText: 'Cancelar'
+      }).subscribe(r => {
+        if (!r) return;
+
+        this.service.update(this.testForm.value, this.register.id).subscribe({next: (r) => {
+            if(r) this.dialog.successDialog({
+              title: 'Actualización exitosa',
+              message: 'Se ha actualizado el registro',
+              acceptText: 'Aceptar'
+            }).subscribe(s => {if(s) this.redirect()})
+          }, error: (e) => {
+            console.log(e)
+            this.dialog.errorDialog({
+              title: 'Ha ocurrido un error',
+              message: 'Tu registro no se ha podido actualizar',
+              acceptText: 'Aceptar'
+            }).subscribe(s => {if(s) this.redirect()})
+          }
+        })
+      })
+    }
   }
+  redirect = () => this.router.navigate(['/tests']);
 }
